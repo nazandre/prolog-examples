68b3d60b78db7aaefc17bcd97411d37395d395ca
Andre Naz <andre.naz01@gmail.com>
2015-12-17 14:04:43 +0100

    graph, some progresses

--- programs/graph.pl
+++ programs/graph.pl
@@ -1,15 +1,17 @@
 /* Source: https://www.cpp.edu/~jrfisher/www/prolog_tutorial/2_15A.pl */
 
 /* graph description */
-edge(A,B).
-edge(A,C).
-edge(C,E).
-edge(E,F).
-edge(B,F).
+edge(a,b).
+edge(a,c).
+edge(c,e).
+edge(e,f).
+edge(b,f).
 
 /* undirected graph (bi-directionnal links) */
 connected(X,Y) :- edge(X,Y) ; edge(Y,X).
 
-
-
+/* path between two vertices */
+%% not correclty tested, may be buggy
+path(X,Y,[X,Y]) :- connected(X,Y).
+path(X,Z,Path) :- connected(Y,Z), Z \= X, path(X,Y,P1), append(P1,[Z],Path).
 
