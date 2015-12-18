3165257355b3fba490e8f2d67a8875991fcfe94f
Andre Naz <andre.naz01@gmail.com>
2015-12-18 16:52:12 +0100

    list: append

--- programs/list.pl
+++ programs/list.pl
@@ -1,9 +1,13 @@
 
 /* length */
 length2([],0).
-length2([H|T],L) :- length2(T,L1), L is L1+1.
+length2([_|T],L) :- length2(T,L1), L is L1+1.
 
 /* maximum */
 max2(X,Y,R) :- X >= Y, R is X; X < Y, R is Y.
 max([],0).
 max([H|T], R) :- max(T,R1), max2(H,R1,R).
+
+/* append */
+append([],L,L).
+append([H|T1],L2,[H|L3]) :- append(T1,L2,L3).
